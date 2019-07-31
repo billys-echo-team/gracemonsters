1ce8ae2cfe331433167a10bfa247409269f8a99c
Alan Tang <atang8013@gmail.com>
2019-07-31 11:31:43 -0400

    seed data and some tests

--- seed.js
+++ seed.js
@@ -5,6 +5,7 @@ const Item = require('./server/db/models/item')
 const seed = async () => {
   try {
     await db.sync({force: true})
+    //*User dummy data
     await User.create({
       userName: 'ashKetchum',
       firstName: 'Ash',
@@ -12,14 +13,59 @@ const seed = async () => {
       email: 'ashe@email.com',
       address: '123 Pallet Town',
       payment: null,
-      password: '123'
+      password: '123',
+      imageUrl:
+        'https://cdn.bulbagarden.net/upload/thumb/5/54/Ash_SM.png/300px-Ash_SM.png'
     })
+    await User.create({
+      userName: 'thirstyBrock',
+      firstName: 'Brock',
+      lastName: 'Takeshi',
+      email: 'brock@email.com',
+      address: '123 Kanto Region',
+      payment: null,
+      password: '456',
+      imageUrl: 'https://cdn.bulbagarden.net/upload/6/6a/Brock_SM.png'
+    })
+    await User.create({
+      userName: 'iamMisty',
+      firstName: 'Misty',
+      lastName: 'Sakura',
+      email: 'misty@email.com',
+      address: '123 Cerulean City',
+      payment: null,
+      password: '789',
+      imageUrl:
+        'https://cdn.bulbagarden.net/upload/thumb/f/f6/Lets_Go_Pikachu_Eevee_Misty.png/367px-Lets_Go_Pikachu_Eevee_Misty.png'
+    })
+    //*Items dummy data
     await Item.create({
       name: 'Bulbasaur',
       element: 'grass',
       price: 20,
       stock: 1,
+      imageUrl:
+        'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/500px-001Bulbasaur.png',
       description: 'its a bulbasaur'
+    })
+
+    await Item.create({
+      name: 'Charmander',
+      element: 'fire',
+      price: 20,
+      stock: 1,
+      imageUrl:
+        'https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/500px-004Charmander.png',
+      description: 'its a charmander'
+    })
+    await Item.create({
+      name: 'Squirtle',
+      element: 'water',
+      price: 20,
+      stock: 1,
+      imageUrl:
+        'https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/500px-007Squirtle.png',
+      description: 'its a squirtle'
     })
     console.log(`
       Seed success!
@@ -35,3 +81,5 @@ const seed = async () => {
 }
 
 seed()
+
+module.exports = seed
