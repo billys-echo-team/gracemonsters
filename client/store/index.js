0158a7fc808c65d9d4942f0a0f5c30fe63f8484c
Chris Choi <chc5139@gmail.com>
2019-08-01 11:46:29 -0400

    fix: throughtable name && store

--- client/store/index.js
+++ client/store/index.js
@@ -1,24 +1,17 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 
-import itemsReducer from './itemsReducer'
+// import itemsReducer from './itemsReducer'
 
-const reducer = combineReducers({ user })  //?? need to have user reduecer....?
+const reducer = combineReducers({user}) //?? need to have user reduecer....?
 
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
-
-const rootReducer = combineReducers({
-  items: itemsReducer,
-  // users: userReducer
-})
-
-export default rootReducer
 
 // Not sure about there are TWO default export
 
